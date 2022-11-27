44f67fc23c581d86ccc27372ed6ea7e11479e31d
danystefanescu <danydavidgt@gmail.com>
2022-11-27 12:36:19 +0000

    Conditional Profile Card terminado con if y diferentes selectores

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,28 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  let yourName = `${variables.name}`;
+  if (variables.name == null) yourName = "Your name & ";
+  let yourLastName = `${variables.lastname}`;
+  if (variables.lastname == null) yourLastName = " lastname";
+  let yourRole = `${variables.role}`;
+  if (variables.role == null) yourRole = "Career";
+  let yourCity = `${variables.city}`;
+  if (variables.city == null) yourCity = "City";
+  let yourCountry = `${variables.country}`;
+  if (variables.country == null) yourCountry = "Country";
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${yourName} ${yourLastName} </h1>
+          <h2>${yourRole}</h2>
+          <h3>${yourCity}, ${yourCountry} </h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="bi bi-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="bi bi-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="bi bi-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="bi bi-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,14 +63,16 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://media.istockphoto.com/id/1189542390/vector/paper-layer-blue-abstract-background-use-for-banner-cover-poster-wallpaper-design-with-space.jpg?s=612x612&w=0&k=20&c=Wc0Qu_dyI6InL6VmCrRI1EhwPBYUIHZsJaBwsu3pgQU=",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://i.pinimg.com/236x/ff/e5/c7/ffe5c7867c2b9529b62a529ffcc567aa--mens-suits-gorgeous-men.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "position-right",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
@@ -85,7 +96,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new valus
+      render(Object.assign(window.variables, values)); // render again the card with new values
     });
   });
 };
